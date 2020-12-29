0acb4579127a67009c2dc8f279ca896ba0fec76a
tpoisonooo <khj.application@aliyun.com>
2020-12-29 17:59:20 +0800

    feat(testcase): add testcase

--- nchw4/build.sh
+++ nchw4/build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -c -g -O0 main.cpp
+g++ -std=c++17 -c -g -O3 -mtune=native main.cpp
 g++ -o main main.o
--- nchw4/main.cpp
+++ nchw4/main.cpp
@@ -3,7 +3,31 @@
 #include <memory>
 #include <cassert>
 #include <string>
-#include <initializer_list>
+#include <vector>
+#include <chrono>
+
+using Clock = std::chrono::high_resolution_clock;
+
+class Timer {
+public:
+    explicit Timer(const std::string& name)
+        : name_(name) {
+        start_ = Clock::now();
+    }
+
+    ~Timer() {
+        stop_ = Clock::now();
+        auto interval = std::chrono::duration_cast<std::chrono::microseconds>(
+                            stop_ - start_)
+                            .count();
+        fprintf(stdout, "%s cost %ld\n", name_.c_str(), interval);
+    }
+
+private:
+    std::string name_;
+    Clock::time_point start_;
+    Clock::time_point stop_;
+};
 
 #define B_LOOP(i, j)\
     for (size_t i = 0; (i) < (j); ++(i)) {
@@ -32,7 +56,7 @@ struct Tensor {
             B_LOOP(j, c)
                 T* ptr = base + j * c_step();
                 B_LOOP(k, w*h)
-                    ptr[k] = k;
+                    ptr[k] = j%2;
                 E_LOOP()
             E_LOOP()
         E_LOOP()
@@ -78,9 +102,14 @@ struct Tensor {
         return h * w;
     }
     
-    T* data_ptr(std::initializer_list<size_t> shape) {
-        assert(not shape.empty());        
-
+    T* ptr_at(const std::vector<size_t>& shape) const {
+        assert(shape.size() == 4);
+        T* ptr = data.get();
+        ptr += shape[0] * n_step();
+        ptr += shape[1] * c_step();
+        ptr += shape[2] * w;
+        ptr += shape[3];
+        return ptr;
     }
 
     size_t n, c, h, w;
@@ -88,8 +117,8 @@ struct Tensor {
 };
 
 template<typename T>
-Tensor<T> nchw_to_nchw4(const Tensor<T> from) {
-    assert(from.c % 4 == 0 and 1 == from.n); 
+Tensor<T> nchw_to_nchw4(const Tensor<T>& from) {
+    assert(from.c % 4 == 0); 
         
     Tensor<T> out(from.n, from.c / 4, from.h, from.w * 4);
     size_t c_loop = from.c / 4;
@@ -116,7 +145,7 @@ Tensor<T> nchw_to_nchw4(const Tensor<T> from) {
 }
 
 template<typename T>
-Tensor<T> nchw4_to_nchw(const Tensor<T> from) {
+Tensor<T> nchw4_to_nchw(const Tensor<T>& from) {
     assert(from.w % 4 == 0);
         
     Tensor<T> out(from.n, from.c * 4, from.h, from.w / 4);
@@ -158,20 +187,124 @@ int test_nchw4_convert() {
     Tensor<float> ker(1, 4, 3, 3);
 }
 
+template<typename T, size_t X>
+Tensor<T> im2colX(const Tensor<T>& in, const Tensor<T>& ker) {
+   Tensor<T> out(1, 1, 1, 1); 
+   return out;
+}
+
 template<typename T>
-Tensor<T> naive_conv_nchw(Tensor<T> in, Tensor<T> ker) {
+Tensor<T> naive_conv_nchw(const Tensor<T>& in, const Tensor<T>& ker) {
     assert(in.c == ker.c); 
     size_t out_h = in.h - ker.h + 1;
     size_t out_w = in.w - ker.w + 1;
     Tensor<T> out(in.n, ker.n, out_h, out_w);
 
-    
+    B_LOOP(on, out.n)
+        B_LOOP(oc, out.c)
+            B_LOOP(oh, out.h)
+                B_LOOP(ow, out.w)
+                    T sum = 0;
+                    for(size_t start_h = oh; start_h < oh + ker.h; ++start_h) {
+                        for(size_t start_w = ow; start_w < ow + ker.w; ++start_w) {
+                            for(size_t ic = 0; ic < in.c; ++ic) {
+                                sum += (*in.ptr_at({on, ic, start_h, start_w})) * (*ker.ptr_at({oc, ic, start_h - oh, start_w - ow})); 
+                            }
+                        }
+                    }
+                    *(out.ptr_at({on, oc, oh, ow})) = sum;
+                E_LOOP()
+            E_LOOP()
+        E_LOOP()
+    E_LOOP()
+    return out;
+}
+
+template<typename T>
+Tensor<T> naive_conv_nchw4(const Tensor<T>& in, const Tensor<T>& ker) {
+    assert(in.c == ker.c); 
+    size_t out_h = in.h - ker.h + 1;
+    size_t out_w = in.w/4 - ker.w/4 + 1;
+    Tensor<T> out(in.n, ker.n, out_h, out_w);
+
+    B_LOOP(on, out.n)
+        B_LOOP(oc, out.c)
+            B_LOOP(oh, out.h)
+                B_LOOP(ow, out.w)
+                    T sum = 0;
+                    for(size_t ic = 0; ic < in.c; ++ic) {
+                        for(size_t start_h = oh; start_h < oh + ker.h; ++start_h) {
+                            for(size_t start_w = ow; start_w < ow + ker.w / 4; start_w += 1) {
+                                const auto in_ptr = in.ptr_at({on, ic, start_h, start_w * 4});
+                                const auto ker_ptr = ker.ptr_at({oc, ic, start_h - oh, (start_w - ow) * 4});
+                                sum += in_ptr[0] * ker_ptr[0];
+                                sum += in_ptr[1] * ker_ptr[1];
+                                sum += in_ptr[2] * ker_ptr[2];
+                                sum += in_ptr[3] * ker_ptr[3];
+                            }
+                        }
+                    }
+                    *(out.ptr_at({on, oc, oh, ow})) = sum;
+                E_LOOP()
+            E_LOOP()
+        E_LOOP()
+    E_LOOP()
+    return out;
 }
 
 int test_convolution() {
-    
+    Tensor<float> in(1, 4, 8, 8);
+    Tensor<float> ker(1, 4, 3, 3);
+    auto out_nchw = naive_conv_nchw(in, ker);
+    out_nchw.print("naive_conv");
+}
+
+int test_convolution_nchw4() {
+    Tensor<float> in(1, 32, 8, 8);
+    auto in_convert = nchw_to_nchw4(in);
+    in_convert.print("in_convert");
+
+    Tensor<float> ker(32, 32, 3, 3);
+    auto ker_convert = nchw_to_nchw4(ker);
+    ker_convert.print("ker_convert");
+
+    auto out_nchw = naive_conv_nchw(in, ker);
+
+    auto out = naive_conv_nchw4(in_convert, ker_convert);
+    out.print("naive_conv_nchw4");
+
+    assert(out_nchw.same(out));
+}
+
+void test_fp32_conv_speedup(size_t in, size_t ic, size_t ih, size_t iw, size_t on, size_t kh, size_t kw, size_t loop) {
+    Tensor<float> _in(in, ic, ih, iw);
+    Tensor<float> _ker(on, ic, kh, kw);
+    {
+        Timer t1("normal");
+
+        B_LOOP(i, loop)
+            naive_conv_nchw(_in, _ker);
+        E_LOOP()
+
+    }
+    {
+        auto _in_convert = nchw_to_nchw4(_in);
+        auto _ker_convert = nchw_to_nchw4(_ker);
+        Timer t2("nchw4");
+
+        B_LOOP(i, loop)
+            naive_conv_nchw4(_in_convert, _ker_convert);
+        E_LOOP()
+    }
 }
 
 int main() {
+    test_nchw4_convert();
+    test_convolution();
+    test_convolution_nchw4();
+    test_fp32_conv_speedup(1, 8, 224, 224, 8, 3, 3, 1);
+    test_fp32_conv_speedup(1, 8, 112, 112, 8, 5, 5, 1);
+    test_fp32_conv_speedup(1, 64, 64, 64, 64, 3, 3, 1);
+    test_fp32_conv_speedup(1, 64, 32, 32, 64, 1, 1, 1);
     return 0;
 }
