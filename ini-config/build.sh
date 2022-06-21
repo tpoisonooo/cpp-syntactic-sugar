d7895c0d162c8490549f983853dcc643f2b74ce5
tpoisonooo <khj.application@aliyun.com>
2022-06-21 16:41:05 +0800

    improvement(ini_config): split template spec

--- ini-config/build.sh
+++ ini-config/build.sh
@@ -1,3 +1,4 @@
 g++ -std=c++14 -g -O0 -c main.cpp
+g++ -std=c++14 -g -O0 -c ini_config.cpp
 
-g++ -o main main.o
+g++ -o main main.o ini_config.o
--- ini-config/ini_config.h
+++ ini-config/ini_config.h
@@ -1,3 +1,17 @@
+// author:tpoisonooo (https://github.com/tpoisonooo/) .
+//
+// Copyright (C) 2022 tpoisonooo. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 #pragma once
 #include <cassert>
 #include <fstream>
@@ -30,16 +44,10 @@ std::string value_set(T data)
 }
 
 template<>
-std::string value_set<std::string>(std::string data)
-{
-    return "\"" + data + "\"";
-}
+std::string value_set<std::string>(std::string data);
 
 template<>
-std::string value_set<const char*>(const char* data)
-{
-    return "\"" + std::string(data) + "\"";
-}
+std::string value_set<const char*>(const char* data);
 
 template<typename T>
 std::string value_set(const std::vector<T>& data)
@@ -72,13 +80,7 @@ T value_get(std::string text)
 }
 
 template<>
-std::string value_get<std::string>(std::string text)
-{
-    auto start = text.find('\"');
-    auto end = text.find_last_of('\"');
-
-    return text.substr(start + 1, end - start - 1);
-}
+std::string value_get<std::string>(std::string text);
 
 /**
  * @brief parse `[1, 2.2]` format to value list
