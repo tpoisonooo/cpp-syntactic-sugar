f8834cc05227bdba9e7890712f4875619580bcc5
tpoisonooo <khj.application@aliyun.com>
2022-06-21 11:15:42 +0800

    improvement(main.cpp): use find_first_of

--- log-int-softmax/main.cpp
+++ log-int-softmax/main.cpp
@@ -33,7 +33,7 @@ static inline int64_t int_exp(int32_t x, float s) {
 }
 
 static inline float fast_pow2_multiply_3(const int32_t index) {
-  assert(index <= 30 && index >=-1);
+  assert(index <= 30 && index >= -1);
   static float table[] = {
       1.5f,       3,         6,         12,        24,        48,
       96,         192,       384,       768,       1536,      3072,
@@ -42,6 +42,37 @@ static inline float fast_pow2_multiply_3(const int32_t index) {
       25165824,   50331648,  100663296, 201326592, 402653184, 805306368,
       1610612736, 3221225472};
   return table[index + 1];
+}
+
+static inline int32_t find_first_one(int32_t v) {
+  int pos = 0;
+
+  if (v > 0xffff) {
+    v >>= 16;
+    pos += 16;
+  }
+
+  if (v > 0xff) {
+    v >>= 8;
+    pos += 8;
+  }
+
+  if (v > 0xf) {
+    v >>= 4;
+    pos += 4;
+  }
+
+  if (v > 0x3) {
+    v >>= 2;
+    pos += 2;
+  }
+
+  if (v > 0x1) {
+    v >>= 1;
+    pos += 1;
+  }
+
+  return pos;
 }
 
 int log_int_softmax_inverse_15(int32_t *ptr, int64_t *buffer, int8_t *out,
@@ -67,7 +98,8 @@ int log_int_softmax_inverse_15(int32_t *ptr, int64_t *buffer, int8_t *out,
   // log_round
   const int QUANT_MAX = 15;
   for (int i = 0; i < len; ++i) {
-    int32_t val = floor(log2(ptr[i]));
+    // int32_t val = floor(log2(ptr[i]));
+    int32_t val = find_first_one(ptr[i]);
     float big = fast_pow2_multiply_3(val - 1);
     // fprintf(stdout, "val %d\n", val);
     if (ptr[i] >= big) {
